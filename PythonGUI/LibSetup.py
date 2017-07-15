fd45d3fa49a199abd1149575e378d8daa8470cdd
Incubator85653 <2bemperor47@gmail.com>
2017-07-15 11:00:47 +0800

    Update API of setup progress 3
    - TODO

--- PythonGUI/LibSetup.py
+++ PythonGUI/LibSetup.py
@@ -3,6 +3,8 @@ from LibOperate import Shortcut
 from LibPython import Environment as Env
 from LibOperate import WaterWellsYaml as wwYaml
 from LibInstallProfile import DecodedProfile as wizardCfg
+from LibPython import Process
+from LibPython import Console
 
 class Methods:
     class CopyFiles:
@@ -39,29 +41,73 @@ class Methods:
         # format.
         # They need a convert from Template(UTF-8) to Target(Local ANSI).
         def update_batching_editor_config():
-            """Copy installer generated data to batching editor profile.
-            This crazy method will tell you sometimes ugly access path is better than nest loop."""
+            """Return edited editor config.
+            This crazy method will tell you sometimes ugly access path is faster than nest loop."""
             # Sumeragi Shion - Gujjo bu
             # A tempoary variable to hold edited dictionary
             shion = wizardCfg.Config.BatchEditorYaml
-
-            # File: Tsk_MainConfig
-            # Modify GameExePath
-            shion['Tsk_MainConfig']['OptionArray'][0]['GameExePath']['Value'] = wizardCfg.Methods.Basic.GameExePath()
             
-            # Modify TskNetExePath
-            shion['Tsk_MainConfig']['OptionArray'][0]['TskNetExePath']['Value'] = wizardCfg.Methods.Installer.Optional.ProgramStructure.TskNet.Bin.TskNetMainExe()
+            # File: Tsk_MainConfig
+            try:
+                inst_path = wizardCfg.Methods.Basic.InstallPath()
+                # Fill installed path
+                source = shion['Tsk_MainConfig']['Source'].format(inst_path)
+                shion['Tsk_MainConfig']['Source'] = source
+                target = shion['Tsk_MainConfig']['Target'].format(inst_path)
+                shion['Tsk_MainConfig']['Target'] = target
+
+                # Modify GameExePath
+                shion['Tsk_MainConfig']['OptionArray'][0]['GameExePath']['Value'] = wizardCfg.Methods.Basic.GameExePath()
+            
+                # Modify TskNetExePath
+                shion['Tsk_MainConfig']['OptionArray'][1]['TskNetExePath']['Value'] = wizardCfg.Methods.Optional.Structure.Program.TskNet.Bin.TskNetMainExe()
+            except Exception as e:
+                
+                Process.handle_exception(e, False)
             
             # File: TskNet_Account
-            # Account and password condition
-            if wizardCfg.Methods.Unattended.ManageId():
-                # Modify TencoAccount
-                shion['TskNet_Account']['OptionArray'][0]['TencoAccount']['Value'] = wizardCfg.Methods.UserData.TencoAccount()
-                # Modify TencoPassword
-                shion['TskNet_Account']['OptionArray'][0]['TencoPassword']['Value'] = wizardCfg.Methods.UserData.TencoPassword()
-            #TODO
+            try:
+                inst_path = wizardCfg.Methods.Basic.InstallPath()
+                # Fill installed path
+                source = shion['TskNet_Account']['Source'].format(inst_path)
+                shion['TskNet_Account']['Source'] = source
+                target = shion['TskNet_Account']['Target'].format(inst_path)
+                shion['TskNet_Account']['Target'] = target
+            
+                # Account and password condition
+                if wizardCfg.Methods.Unattended.ManageId():
+                    # Modify TencoAccount
+                    shion['TskNet_Account']['OptionArray'][0]['TencoAccount']['Value'] = wizardCfg.Methods.UserData.TencoAccount()
+                    # Modify TencoPassword
+                    shion['TskNet_Account']['OptionArray'][1]['TencoPassword']['Value'] = wizardCfg.Methods.UserData.TencoPassword()
+            except Exception as e:
+                
+                Process.handle_exception(e, False)
+
+            return shion
+        def get_saved_editor_config_path():
+            try:
+                inst_path = wizardCfg.Methods.Basic.InstallPath()
+                edited_yaml = Methods.EditConfigs.update_batching_editor_config()
+                saved_path = Env.Path.Complement.merge_system(inst_path, "Templates\BatchEditor.yaml")
+            
+                wwYaml.write_yaml_to_disk(edited_yaml, saved_path)
+            except Exception as e:
+                
+                Process.handle_exception(e, False)
+
+            return saved_path
+        def run_batch_editor():
+            inst_path = wizardCfg.Methods.Basic.InstallPath()
+            editor_exe = Env.Path.Complement.merge_system(inst_path, "Templates\BatchEditor.exe")
+            editor_config = Methods.EditConfigs.get_saved_editor_config_path()
+            editor_resource = inst_path
+            
+            editor_command = "\"{}\" \"{}\" \"{}\"".format(editor_exe, editor_config, editor_resource)
+            Console.ansi_print(Process.get_command_exit_code(editor_command, True))
         def wrapper_do_edit():
             Methods.EditConfigs.update_batching_editor_config()
+            Methods.EditConfigs.run_batch_editor()
             #TODO
     class Shortcuts:
         def ShortcutRules(Action, FileName, Args):
@@ -165,7 +211,7 @@ def Wrapper_NewInstall():
         # Turn all install success var to True after all operation success.
         all_install_success = True
     except Exception as e:
-        from LibPython import Process
+        
         Process.handle_exception(e, False)
 
     return all_install_success
